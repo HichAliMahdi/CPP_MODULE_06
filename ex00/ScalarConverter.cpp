9cf15b7877f3ed60bf26d7d4dcafa07da3e5b807
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-6.42heilbronn.de>
2025-07-15 13:20:20 +0200

    Did the detectType part for detecting the origin of the format

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,9 +6,30 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:42:52 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/07/15 12:43:07 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/07/15 13:19:49 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+ScalarConverter::LiteralType ScalarConverter::detectType(const std::string& literal)
+{
+    if (literal.length() == 3 && literal[0] == '\'' && literal[2] == '\'')
+        return CHAR_TYPE;
+    if (literal == "nanf" || literal == "+inff" || literal == "-inff")
+        return FLOAT_TYPE;
+    if (literal == "nan" || literal == "+inf" || literal == "-inf")
+        return DOUBLE_TYPE;
+    if (literal.find('.') != std::string::npos)
+    {
+        if (literal.back() == 'f' || literal.back() == 'F')
+            return FLOAT_TYPE;
+        else
+            return DOUBLE_TYPE;
+    }
+    std::stringstream ss(literal);
+    int temp;
+    if (ss >> temp && ss.eof())
+        return INT_TYPE;
+    return INVALID_TYPE;
+}
