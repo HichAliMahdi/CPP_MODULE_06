04e8c92edc5de272cb25f657a2bcdf04dcf6756e
Hichem Abderrahmane Ali Mahdi <hali-mah@1-A-32.42heilbronn.de>
2025-07-19 21:55:10 +0200

    added main to ex00, fixing stuff after testing

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,19 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 21:44:04 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/07/19 21:44:05 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/07/19 21:52:00 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <literal>" << std::endl;
+        return 1;
+    }
+    ScalarConverter::convert(argv[1]);
+    return 0;
+}
