b662dce39a029e9ce4c9f714ada788f3549c3605
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-7.42heilbronn.de>
2025-07-20 16:10:11 +0200

    Added the makefile and sometests to ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 15:55:57 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/07/20 16:01:02 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/07/20 16:03:06 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,4 +85,46 @@ void identify(Base& p)
         return;
     }
     catch (...) {}
+}
+
+int main()
+{
+    std::cout << "=== Testing dynamic_cast with pointers and references ===" << std::endl;
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << "\nTest " << (i + 1) << ":" << std::endl;
+        Base* obj = generate();
+        std::cout << "Identify by pointer: ";
+        identify(obj);
+        std::cout << "Identify by reference: ";
+        identify(*obj);
+        delete obj;
+        std::cout << std::endl;
+    }
+    
+    std::cout << "=== Testing specific object types ===" << std::endl;
+    
+    // Test each class specifically
+    Base* a = new A();
+    Base* b = new B();
+    Base* c = new C();
+    
+    std::cout << "\nTesting A object:" << std::endl;
+    std::cout << "Pointer: "; identify(a);
+    std::cout << "Reference: "; identify(*a);
+    
+    std::cout << "\nTesting B object:" << std::endl;
+    std::cout << "Pointer: "; identify(b);
+    std::cout << "Reference: "; identify(*b);
+    
+    std::cout << "\nTesting C object:" << std::endl;
+    std::cout << "Pointer: "; identify(c);
+    std::cout << "Reference: "; identify(*c);
+    
+    std::cout << "\nCleaning up..." << std::endl;
+    delete a;
+    delete b;
+    delete c;
+    
+    return 0;
 }
